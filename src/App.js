41b3aee0b891edb27eaccd8e8b57f5df08247ed8
isaacasher97 <isaac.asher97@gmail.com>
2023-07-25 21:10:39 -0400

    props exercise begun, first exercise complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+//child element called Books
+const Books = ({bookTitle, author, alt, src, width, height}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <h2>{bookTitle}</h2>
+    <span className="authorName"><h3>{author}</h3></span>
+    <img 
+    src= {src}
+    alt= {alt}
+    width= {width}
+    height= {height}
+    />
+  </>
+)
+}
+
+
+const App = () => {
+  return (
+      <div id="booksContainer">
+      <div className="books">  
+      <Books
+        bookTitle= "Harry Potter and the Sorcerer's Stone"
+        src= "https://books.google.com/books/content?id=wrOQLV6xB-wC&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U0pxFjDUW9HplCcIzSmlQs0B159ow&w=1280"
+        width= {200}
+        height= {300}
+      />
+      <Books
+        bookTitle= "Harry Potter and the Chamber of Secrets"
+        src= "https://books.google.com/books/content?id=5iTebBW-w7QC&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U23kQWHmyYKWnZZ2kLaka0zvjyjGg&w=1280"
+        width= {200}
+        height= {300}
+      />
+      <Books
+        bookTitle= "Harry Potter and the Prisoner of Azkaban"
+        src= "https://books.google.com/books/content?id=Sm5AKLXKxHgC&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U3tmtZXHnvR_e5jlMSAFaXT9WIuuA&w=1280"
+        width= {200}
+        height= {300}
+        author="Written By: J.K. Rowling"
+      />
+      </div>
+      </div>
   );
 }
 
